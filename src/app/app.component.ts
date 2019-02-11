53dae32d44ccaf982047796b8eefc3efa3b5ed41
Dwight Bell <dwight.bell1@gmail.com>
2019-02-10 18:07:46 -0800

    Not sure, whatever I did before we left for Ash's

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,57 @@
 import { Component } from '@angular/core';
-// import {
-// stuff
-// } from '@angular/animations';
+import {
+  transition,
+  trigger,
+  query,
+  style,
+  animate,
+  group,
+  animateChild
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  template: '<div [@animRoutes]="getPage(appOutlet)"><router-outlet #appOutlet="outlet"></router-outlet></div>',
   styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('animRoutes', [
+      transition('* <=> *', [
+        group([
+          query(
+            ':enter',
+            [
+              style({
+                opacity: 0,
+                transform: 'translateY(9rem) rotate(-10deg)'
+              }),
+              animate(
+                '0.35s cubic-bezier(0, 1.8, 1, 1.8)',
+                style({ opacity: 1, transform: 'translateY(0) rotate(0)' })
+              ),
+              animateChild()
+            ],
+            { optional: true }
+          ),
+          query(
+            ':leave',
+            [animate('0.35s', style({ opacity: 0 })), animateChild()],
+            { optional: true }
+          )
+        ])
+      ])
+    ])
+  ]
 })
 export class AppComponent {
     title = 'angular-spa';
+
+    getPage(outlet) {
+      return outlet.activatedRouteData.page || 'one';
+    }
 }
 
 @Component({
   templateUrl: './templates/home.component.html',
-  animations: [],
 })
 export class HomeComponent {
     title = 'Home';
